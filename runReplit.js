ab6b90607619238b88bddbb8b727eaab50447c63
fastapp24h <FASTAPP24H@GMAIL.COM>
2025-03-31 14:53:44 +0200

    Update runReplit.js

--- runReplit.js
+++ runReplit.js
@@ -4,7 +4,6 @@ const puppeteer = require('puppeteer-core');
 module.exports = async function runReplit(prompt) {
   let browser;
   try {
-    console.log('Lancio browser');
     browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -13,19 +12,15 @@ module.exports = async function runReplit(prompt) {
     });
 
     const page = await browser.newPage();
-    console.log('Navigo su Replit');
     await page.goto('https://replit.com/~', { waitUntil: 'networkidle2' });
 
-    console.log('Attendo pulsante "Create"');
     await page.waitForSelector('button:has-text("Create")', { timeout: 15000 });
     await page.click('button:has-text("Create")');
 
-    console.log('Inserisco prompt');
     await page.waitForSelector('textarea[placeholder*="What would you like to make"]', { timeout: 15000 });
     await page.type('textarea[placeholder*="What would you like to make"]', prompt, { delay: 30 });
     await page.keyboard.press('Enter');
 
-    console.log('Attendo iframe webview');
     await page.waitForSelector('iframe', { timeout: 60000 });
 
     const finalUrl = await page.evaluate(() => {
@@ -34,10 +29,8 @@ module.exports = async function runReplit(prompt) {
     });
 
     await browser.close();
-    console.log('Link generato:', finalUrl);
     return finalUrl;
   } catch (err) {
-    console.error('Errore interno Puppeteer:', err);
     if (browser) await browser.close();
     throw new Error('Errore interno Puppeteer: ' + err.message);
   }
