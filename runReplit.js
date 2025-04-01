ce23927cc1a8b4964d1b8767d7275913f7cac647
fastapp24h <FASTAPP24H@GMAIL.COM>
2025-04-01 22:44:04 +0200

    Update runReplit.js

--- runReplit.js
+++ runReplit.js
@@ -1,14 +1,11 @@
-const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer');
 
 module.exports = async function runReplit(prompt) {
   let browser;
   try {
     browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
@@ -19,16 +16,15 @@ module.exports = async function runReplit(prompt) {
     // Aspetta un elemento per confermare che la pagina è caricata
     await page.waitForSelector('header', { timeout: 15000 });
 
-    // Log di debug (opzionale)
     console.log('Pagina caricata:', page.url());
 
-    // Chiudi il browser e restituisci la URL attuale
     await browser.close();
     return page.url();
-    
+
   } catch (err) {
     if (browser) await browser.close();
     console.error('Errore completo Puppeteer:', err);
     throw new Error('Errore durante l\'esecuzione di Puppeteer: ' + err.message);
   }
+};
 };
